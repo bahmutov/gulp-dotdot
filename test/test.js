0c93981d3aa0e4f9a7367b1dfb41062b5de83e2b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-01-18 00:00:32 -0500

    working tests, what is gulp exit code for broken build?

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
-var path = require('path');
+var join = require('path').join;
+var gulp = join(__dirname, '../node_modules/gulp/bin/gulp.js');
 
 gt.module('gulp-dotdot', {
   teardown: process.chdir.bind(process, __dirname)
 });
 
 gt.async('.. -> gulp-dotdot is ok for jshint', function () {
-  process.chdir(path.join(__dirname, 'with-dotdot'));
-  gt.exec('gulp', ['lint'], 0, 'gulp-jshint breaks on .. notation');
+  process.chdir(join(__dirname, 'with-dotdot'));
+  gt.exec('node', [gulp], 0, 'gulp-jshint breaks on .. notation');
 });
 
 gt.async('.. notation breaks jshint', function () {
-  process.chdir(path.join(__dirname, 'without-dotdot'));
-  gt.exec('gulp', [], 1, 'gulp-jshint breaks on .. notation');
+  process.chdir(join(__dirname, 'without-dotdot'));
+  gt.exec('node', [gulp], 1, 'gulp-jshint breaks on .. notation');
 });
